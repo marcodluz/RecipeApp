8692c675749d02b8882b51e4ef338af35465c229
Marco Luz <marcotdl@hotmail.com>
2023-01-07 18:09:02 +0000

    Title

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState("");
+  const [accountList, setAccountList] = useState([]);
+
+  function accountInputHandler(enteredText) {
+    setEnteredText(enteredText);
+  }
+
+  function addAccountHandler() {
+    console.log(enteredText);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.middleView}>
+        <Text style={styles.textTitle}>Recipe Application</Text>
+        <TextInput
+          placeholder="Full Name"
+          style={styles.textInput}
+          onChangeText={accountInputHandler}
+        ></TextInput>
+        <TextInput placeholder="Email" style={styles.textInput}></TextInput>
+        <Button title="Create Account" onPress={addAccountHandler} />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  middleView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 120,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    padding: 20,
+    width: "70%",
+    marginBottom: 10,
+    borderRadius: 10,
+  },
+  textTitle: {
+    padding: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
